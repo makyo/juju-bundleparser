f40bc7617231404914c3ac7370800107354b3927
Francesco Banconi <francesco.banconi@canonical.com>
2015-04-14 11:51:26 +0200

    wip

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -8,6 +8,7 @@ test_parse
 Tests for `parse` module.
 """
 
+from collections import OrderedDict
 import unittest
 
 from bundleparser import parse
@@ -81,9 +82,8 @@ class TestChangeSet(unittest.TestCase):
     def test_send_receive(self):
         self.cs.send('foo')
         self.cs.send('bar')
-        self.assertEqual(self.cs._changeset, ['foo', 'bar'])
         self.assertEqual(self.cs.recv(), ['foo', 'bar'])
-        self.assertEqual(self.cs._changeset, [])
+        self.assertEqual([], self.cs.recv())
 
 
 class TestParse(unittest.TestCase):
@@ -105,11 +105,8 @@ class TestParse(unittest.TestCase):
             'relations': {},
             'series': 'trusty',
         }
-        accumulated = []
-        for i in parse.parse(bundle, handler=self.handler1):
-            accumulated.append(i)
+        changes = list(parse.parse(bundle, handler=self.handler1))
         self.assertEqual(
-            accumulated,
             [
                 (1, 0),
                 (1, 1),
@@ -117,8 +114,117 @@ class TestParse(unittest.TestCase):
                 (2, 0),
                 (2, 1),
                 (2, 2),
-            ]
+            ],
+            changes,
         )
 
-if __name__ == '__main__':
-    unittest.main()
+
+class TestHandleServices(unittest.TestCase):
+
+    def test_handler(self):
+        cs = parse.ChangeSet({
+            # Use an ordered dict so that changes' ids can be predicted
+            # deterministically.
+            'services': OrderedDict((
+                ('django', {
+                    'charm': 'cs:trusty/django-42',
+                }),
+                ('mysql-master', {
+                    'charm': 'cs:utopic/mysql-47',
+                }),
+                ('mysql-slave', {
+                    'charm': 'cs:utopic/mysql-47',
+                    'options': {
+                        'key1': 'value1',
+                        'key2': 'value2',
+                    }
+                }),
+            ))
+        })
+        handler = parse.handle_services(cs)
+        self.assertEqual(parse.handle_machines, handler)
+        self.assertEqual(
+            [
+                {
+                    'id': 'addCharm-0',
+                    'method': 'addCharm',
+                    'args': ['cs:trusty/django-42'],
+                    'requires': []
+                },
+                {
+                    'id': 'addService-1',
+                    'method': 'deploy',
+                    'args': ['cs:trusty/django-42', 'django', {}],
+                    'requires': ['addCharm-0']
+                },
+                {
+                    'id': 'addCharm-2',
+                    'method': 'addCharm',
+                    'args': ['cs:utopic/mysql-47'],
+                    'requires': []
+                },
+                {
+                    'id': 'addService-3',
+                    'method': 'deploy',
+                    'args': ['cs:utopic/mysql-47', 'mysql-master', {}],
+                    'requires': ['addCharm-2']
+                },
+                {
+                    'id': 'addService-4',
+                    'method': 'deploy',
+                    'args': ['cs:utopic/mysql-47', 'mysql-slave', {
+                        'key1': 'value1',
+                        'key2': 'value2',
+                    }],
+                    'requires': ['addCharm-2']
+                },
+            ],
+            cs.recv())
+
+    def test_no_services(self):
+        cs = parse.ChangeSet({'services': {}})
+        parse.handle_services(cs)
+        self.assertEqual([], cs.recv())
+
+
+class TestHandleMachines(unittest.TestCase):
+
+    def test_handler(self):
+        cs = parse.ChangeSet({
+            # Use an ordered dict so that changes' ids can be predicted
+            # deterministically.
+            'machines': OrderedDict((
+                ('1', {'series': 'vivid'}),
+                ('2', {}),
+                ('42', {'constraints': {'cpu-cores': 4}}),
+            ))
+        })
+        handler = parse.handle_machines(cs)
+        self.assertEqual(parse.handle_units, handler)
+        self.assertEqual(
+            [
+                {
+                    'id': 'addMachine-0',
+                    'method': 'addMachine',
+                    'args': ['vivid', {}],
+                    'requires': []
+                },
+                {
+                    'id': 'addMachine-1',
+                    'method': 'addMachine',
+                    'args': ['', {}],
+                    'requires': []
+                },
+                {
+                    'id': 'addMachine-2',
+                    'method': 'addMachine',
+                    'args': ['', {'cpu-cores': 4}],
+                    'requires': []
+                },
+            ],
+            cs.recv())
+
+    def test_no_machines(self):
+        cs = parse.ChangeSet({'services': {}})
+        parse.handle_machines(cs)
+        self.assertEqual([], cs.recv())
